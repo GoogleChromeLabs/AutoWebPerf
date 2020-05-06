c8600408a823b363f50c6b43d99c9079603db437
Jon Chen <jonoson@gmail.com>
2020-05-07 00:49:18 +0800

    Added GCPHandler file with BigQuery Handler class to handle queries with different environments.

---
+++ src/helpers/gcp-handler.js
@@ -0,0 +1,132 @@
+const assert = require('../utils/assert');
+
+/**
+ * The handler for Google Cloud BigQuery with support of multiple platforms
+ * including local Node.js, GCP and AppScript-based environments
+ * (e.g. GoogleSheets.)
+ *
+ * Reference:
+ * - BigQuery with Node.js - https://cloud.google.com/bigquery/docs/reference/libraries
+ * - BigQuery with AppScript - https://developers.google.com/apps-script/advanced/bigquery
+ */
+class BigQueryHandler {
+  constructor(config, apiHandler, options) {
+    this.platform = config.platform || 'Node';
+    this.projectId = config.projectId;
+    this.keyFilename = config.keyFilename;
+
+    switch(this.platform.toLowerCase()) {
+      case 'gcp':
+      case 'node':
+        // Create GCP client for each requested GCP product.
+        let gcpConfig = {
+          projectId: this.projectId,
+          keyFilename: this.keyFilename,
+        };
+
+        const BigQueryModule = require('@google-cloud/bigquery');
+        this.client = new BigQueryModule.BigQuery(gcpConfig);
+        break;
+
+      case 'appscript':
+        // Do nothing.
+        break;
+
+      default:
+        throw new Error(`${this.platform}  is not supported.`);
+        break;
+    }
+  }
+
+  /**
+   * Run BigQuery query and return the array of rows.
+   * @param {string} query Query string in SQL format.
+   * @param {Object} options Options object.
+   * @return {Array<object>} Rows of queried results.
+   *
+   * Available options:
+   * - bigQueryOptions {object}: The options object to be passed to BigQuery
+   *     functions.
+   * - timeout {number}: The timeout limit in milliseconds for all BigQuery
+   *     functions. Default 5000 ms.
+   * - verbose {boolean}: Whether to print verbose logs.
+   */
+  async query(query, options) {
+    assert(query, 'query is missing.');
+    options = options || {};
+    let timeout = options.timeout || 1000 * 60 * 5; // Default 5 minutes.
+
+    switch(this.platform.toLowerCase()) {
+      case 'gcp':
+      case 'node':
+        {
+          // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+          let bigQueryOptions = {
+            query: query,
+          }
+
+          // Run the query as a job
+          const [job] = await this.client.createQueryJob(bigQueryOptions);
+
+          // Wait for the query to finish
+          const [rows] = await job.getQueryResults();
+          return rows;
+        }
+        break;
+
+      case 'appscript':
+        {
+          let request = {
+            query: query,
+          };
+          let queryResults = BigQuery.Jobs.query(request, this.projectId);
+          let jobId = queryResults.jobReference.jobId;
+
+          if (options.verbose) {
+            console.log(`Starting BigQuery job, id = ${jobId}`);
+          }
+
+          // Check on status of the Query Job.
+          let sleepTimeMs = 500;
+          let startMs = new Date().getTime();
+          let timeElapseMs = 0;
+
+          while (!queryResults.jobComplete && timeElapseMs < timeout) {
+            Utilities.sleep(sleepTimeMs);
+            sleepTimeMs *= 2;
+            queryResults = BigQuery.Jobs.getQueryResults(this.projectId, jobId);
+
+            // Check if timeout.
+            timeElapseMs = new Date().getTime() - startMs;
+            if (timeElapseMs >= timeout) throw new Error('BigQuery timeout');
+          }
+
+          // Get all the rows of results.
+          startMs = new Date().getTime();
+          let rows = queryResults.rows;
+
+          while (queryResults.pageToken && timeElapseMs < timeout) {
+            queryResults = BigQuery.Jobs.getQueryResults(
+                this.projectId, jobId, {
+                  pageToken: queryResults.pageToken
+                });
+            rows = rows.concat(queryResults.rows);
+
+            // Check if timeout.
+            timeElapseMs = new Date().getTime() - startMs;
+            if (timeElapseMs >= timeout) throw new Error('BigQuery timeout');
+          }
+          return rows;
+        }
+        break;
+
+      default:
+        throw new Error(`${this.platform}  is not supported.`);
+        break;
+    }
+  }
+}
+
+module.exports = {
+  BigQueryHandler,
+};
---
+++ test/helpers/gcp-handler.test.js
@@ -0,0 +1,97 @@
+/**
+ * @license Copyright 2020 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+'use strict';
+
+const {BigQueryHandler} = require('../../src/helpers/gcp-handler');
+
+describe('BigQueryHandler test', () => {
+  it('creates corresponding BigQuery client in Node.js platform.', async () => {
+    // Use default platform as Node.
+    let handler = new BigQueryHandler({
+      projectId: 'fake-project',
+    });
+    expect(handler.client).not.toBe(undefined);
+  });
+
+  it('perfroms query through BigQuery client in Node.js platform', async () => {
+    let handler = new BigQueryHandler({
+      projectId: 'fake-project',
+      platform: 'Node',
+    });
+
+    handler.client.createQueryJob = () => [{
+      getQueryResults: () => [[{
+        date: '202001', value: 123
+      }, {
+        date: '202002', value: 456,
+      }]],
+    }];
+
+    let rows = await handler.query('fake query SQL');
+    expect(rows.length).toEqual(2);
+  });
+
+  it('perfroms query through BigQuery client in AppScript platform', async () => {
+    let remainingPages = 3;
+    let getPageToken = () => {
+      if (remainingPages <= 0) {
+        return null;
+      } else {
+        remainingPages--;
+        return `pageToken-${remainingPages}`;
+      }
+    }
+
+    global.BigQuery = {
+      Jobs: {
+        query: () => ({
+          jobReference: {
+            jobId: 'fake-id',
+          }
+        }),
+        getQueryResults: () => ({
+          jobReference: {
+            jobId: 'fake-id',
+          },
+          jobComplete: true,
+          pageToken: getPageToken(),
+          rows: [{
+            date: '202001', value: 123
+          }, {
+            date: '202002', value: 456,
+          }],
+        }),
+      }
+    };
+    global.Utilities = {
+      sleep: jest.fn(),
+    };
+
+    let handler = new BigQueryHandler({
+      projectId: 'fake-project',
+      platform: 'AppScript',
+    });
+
+    let rows = await handler.query('fake query SQL');
+    expect(rows.length).toEqual(8);
+  });
+
+  it('throws error when missing projectId.', async () => {
+    expect(() => {
+      let handler = new BigQueryHandler();
+    }).toThrow(Error);
+  });
+
+  it('throws error when initializing a unsupported platform.', async () => {
+    expect(() => {
+      let handler = new BigQueryHandler({
+        projectId: 'fake-project',
+        platform: 'NonSupported',
+      });
+    }).toThrow(Error);
+  });
+
+});
